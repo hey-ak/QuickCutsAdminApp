ab50f69b80fc91e673d98bb354b06ea17ecde4fc
Neeraj Sharma <ns98054978@gmail.com>
2024-05-01 15:11:43 +0530

    added booking page

---
+++ QuickCutsAdminApp/QuickCutsAdminApp/Xibs/BookingCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookingCollectionCell.swift
+//  Quick Cuts
+//
+//  Created by Amit Kumar Dhal on 21/04/24.
+//
+
+import UIKit
+
+class BookingCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var cancelServiceButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
